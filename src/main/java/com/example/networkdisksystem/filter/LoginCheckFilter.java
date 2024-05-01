59cde9c4c9121036b26c3006fe3038139b5c573b
zenglian <1813890249@qq.com>
2024-05-01 15:38:21 +0800

    DDL更新-用户状态新增

--- src/main/java/com/example/networkdisksystem/filter/LoginCheckFilter.java
+++ src/main/java/com/example/networkdisksystem/filter/LoginCheckFilter.java
@@ -1,10 +1,11 @@
 package com.example.networkdisksystem.filter;
 
+import com.example.networkdisksystem.entity.Users;
 import com.example.networkdisksystem.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.ObjectUtils;
-
 import javax.annotation.Resource;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -19,6 +20,9 @@ public class LoginCheckFilter implements Filter {
 
     @Resource
     UserService userService;
+
+    @Resource
+    StringRedisTemplate template;
 
 
     //路径匹配器，支持通配符
@@ -41,38 +45,66 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-
-
         //获取session中的用户信息判断是否登录
         if(request.getSession().getAttribute("user")!= null){
-            long id = Thread.currentThread().getId();
-            log.info("用户已登录，放行！");
-            log.info("{}的线程ID为：{}",url,id);
-            chain.doFilter(request,response);
+            //判断用户状态
+            Users users= (Users) request.getSession().getAttribute("user");
+            String condition = template.opsForValue().get("userCondition" + users.getUid());
+            //状态过期
+            if(ObjectUtils.isEmpty(condition)){
+                //重新获取状态
+                int loginCheck = userService.to_loginCheck(users.getUsername(), users.getPassword());
+                if (loginCheck == 2) {
+                    log.info("用户：{} -- 免密登录", users.getUsername());
+                    chain.doFilter(request, response);
+                    return;
+                }else if (loginCheck == 1){
+                    log.info("用户：{} -- 用户名密码错误", users.getUsername());
+                }else if (loginCheck == 0){
+                    log.info("用户：{} -- 禁用中！", users.getUsername());
+                }else {
+                    log.info("系统异常");
+                }
+            }else {
+                if(condition.equals("0")){
+                    log.info("用户被禁用！");
+                }else {
+                    long id = Thread.currentThread().getId();
+                    log.info("用户已登录，放行！");
+                    log.info("{}的线程ID为：{}",url,id);
+                    chain.doFilter(request,response);
+                    return;
+                }
+            }
+            response.sendRedirect("/user/login");
             return;
         }
 
         //检查cookie中是否存了用户名密码
         Cookie[] cookies = request.getCookies();
         if(!ObjectUtils.isEmpty(cookies)) {
-            String username = null;
-            String password = null;
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("NetWorkDiskSystem_username")) {
-                    username = cookie.getValue();
-                }
-                if (cookie.getName().equals("NetWorkDiskSystem_password")) {
-                    password = cookie.getValue();
-                }
+          String username = null;
+          String password = null;
+          for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("NetWorkDiskSystem_username")) {
+              username = cookie.getValue();
             }
-            if (username != null && password != null) {
-                boolean loginCheck = userService.to_loginCheck(username, password);
-                if (loginCheck) {
-                    log.info("用户：{} -- 免密登录", username);
-                    chain.doFilter(request, response);
-                    return;
-                }
+            if (cookie.getName().equals("NetWorkDiskSystem_password")) {
+              password = cookie.getValue();
             }
+          }
+          if (username != null && password != null) {
+            int loginCheck = userService.to_loginCheck(username, password);
+            if (loginCheck == 2) {
+              log.info("用户：{} -- 免密登录", username);
+              chain.doFilter(request, response);
+              return;
+            }else if (loginCheck == 1){
+                log.info("用户：{} -- 用户名密码错误", username);
+            }else if (loginCheck == 0){
+                log.info("用户：{} -- 禁用中！", username);
+            }
+          }
         }
         log.info("用户未登录");
         response.sendRedirect("/user/login");
--- src/main/java/com/example/networkdisksystem/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/networkdisksystem/service/serviceImpl/UserServiceImpl.java
@@ -3,14 +3,15 @@ package com.example.networkdisksystem.service.serviceImpl;
 import com.example.networkdisksystem.entity.Users;
 import com.example.networkdisksystem.mapper.UserMapper;
 import com.example.networkdisksystem.service.UserService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
-
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,13 +21,25 @@ public class UserServiceImpl implements UserService {
     @Resource
     HttpSession session;
 
+    @Resource
+    StringRedisTemplate template;
+
+
     @Override
-    public boolean LoginCheck(String username, String password, boolean rememberMe,HttpServletResponse response){
+    public int LoginCheck(String username, String password, boolean rememberMe,HttpServletResponse response){
         Users user = mapper.getUsersByUsername(username);
         if(!ObjectUtils.isEmpty(user)){
             String MD5Password = user.getPassword();
             boolean flag = new BCryptPasswordEncoder().matches(password, MD5Password);
             if(flag){
+                //判断登录状态
+                int condition = user.getCondition();
+                if(condition == 0){
+                    return 0;
+                }else {
+                    //将用户状态放入redis中
+                    template.opsForValue().set("userCondition" + user.getUid(),""+1,10,TimeUnit.MINUTES);
+                }
                 //登录成功将用户信息放入session中
                 session.setAttribute("user",user);
                 session.setAttribute("mid",user.getMid());
@@ -43,30 +56,39 @@ public class UserServiceImpl implements UserService {
                     response.addCookie(cookie_username);
                     response.addCookie(cookie_password);
                 }
-                return true;
+                return 2;
             } else {
-                return false;
+                return 1;
             }
         }else {
-            return false;
+            return 1;
         }
     }
 
     @Override
-    public boolean to_loginCheck(String username, String password) {
+    public int to_loginCheck(String username, String password) {
         Users user = mapper.getUsersByUsername(username);
         if(!ObjectUtils.isEmpty(user)) {
             String MD5Password = user.getPassword();
             //boolean flag = new BCryptPasswordEncoder().matches(password, MD5Password);
             boolean flag=MD5Password.equals(password);
             if (flag) {
+                //判断登录状态
+                int condition = user.getCondition();
+                if(condition == 0){
+                    return 0;
+                }else {
+                    //将用户状态放入redis中
+                    template.opsForValue().set("userCondition" + user.getUid(),""+1,10, TimeUnit.MINUTES);
+                }
+                //登录成功将用户信息放入session中
                 session.setAttribute("user", user);
                 session.setAttribute("mid",user.getMid());
-                return true;
+                return 2;
             }else {
-                return false;
+                return 1;
             }
         }
-        return false;
+        return 1;
     }
 }
